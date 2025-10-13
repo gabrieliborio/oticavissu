7a1ab23d8c9f3a0a07ffb1715135ded95f2e3cb9
Gabriel Policarpo <gabrieliborio.pro@gmail.com>
2025-10-13 17:55:48 -0300

    Branding: rename to Le Optique

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.merrylynxglow',
-  appName: 'merry-lynx-glow',
+  appId: 'com.example.leoptique',
+  appName: 'Le Optique',
   webDir: 'dist'
 };
 
